092562d3be466bcb8d2782ba6d5c4eca102530f9
badushaebrahim <63869474+badushaebrahim@users.noreply.github.com>
2021-05-09 15:16:49 +0530

    php test

---
+++ fot.php
@@ -0,0 +1,22 @@
+<?php
+<head>
+<style>
+footer{
+    background-color:balck;
+    color:white;
+}
+</style>
+.imgg{
+    max-width:35%;
+    max-height:35%;
+}
+</style>
+<footer>
+<div>
+<img src="image\iconmonstr-linkedin-3.svg" class="imgg">
+</div>
+
+</footer>
+
+
+?>
