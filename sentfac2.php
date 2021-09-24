274502c2222aba64ea1df3d26994aa8e75916ccb
badushaebrahim <63869474+badushaebrahim@users.noreply.github.com>
2021-09-24 21:33:28 +0530

    messageing setup

--- sentfac2.php
+++ sentfac2.php
@@ -1,10 +1,11 @@
-<!DOCTYPE html><!--seenting msg from student to faculty-->
+<!DOCTYPE html><!--fac msg student-->
+<?php include './functionsphp/dbcheck.inc.php'; ?>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>mesage</title>
+	<title>faculty  to Stuent</title>
 	<script src="./js/jquery-2.1.4.min.js">
 	</script>
     <link href="css/msg.css" rel="stylesheet" type="text/css">
@@ -13,23 +14,37 @@
 <body>
 	<div class="maincontainer">
 <div class="msgholder">
+<?php
+$re=1;
+ $l = "SELECT * FROM `msgholder` WHERE `reciverid`='$re' and `sendertype`=2";
+ $sop=mysqli_query($conn,$l);
+echo "<div class ='ms'> "; 
+ while ($row2 =mysqli_fetch_array($sop)) {
+    unset($username);echo"<divclass='msgss'>";
+    $username = $row2['message'];
+    $tid = $row2['uid'];
+    echo '<h3>'.$username.'</h3></div><br>';
+}
+
+echo"<div>";
+?>
 </div>
 
 <br>
-<form method="POST" action="sent.inc.php">
+<form method="POST" action="sentfactopar.php">
 
 </div><br><br>
 <div class="butsholder">
 <?php
-include './functionsphp/dbcheck.inc.php';
-$q="select name,tid from faclog";
+
+$q="select name,uid from studlog";
  $s=mysqli_query($conn,$q);
-echo "<select name='faculty' >";
-echo"<option  value=''>names of faculty</options>";
+echo "<select name='faculty' >"; 
+echo"<option  value=''>names of Student</options>";
  while ($row =mysqli_fetch_array($s)) {
     unset($username);
     $username = $row['name'];
-    $tid = $row['tid'];
+    $tid = $row['uid'];
     echo '<option value="'.$tid.'"  >'.$username.'</option>';
 }
 echo "</select>";
