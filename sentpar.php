4966df0a09a6157cfaeebe257a986bbd20777c5e
badushaebrahim <63869474+badushaebrahim@users.noreply.github.com>
2021-10-09 22:20:17 +0530

    before change in studcls

--- sentpar.php
+++ sentpar.php
@@ -15,7 +15,8 @@
 	<div class="maincontainer">
 <div class="msgholder">
 <?php
-$re=$_SESSION['pid'];
+session_start();
+$re=$_SESSION['pid2'];
  $l = "SELECT * FROM `msgholder` WHERE `reciverid`='$re' and `sendertype`=6";
  $sop=mysqli_query($conn,$l);
 echo "<div class ='ms'> "; 
@@ -58,18 +59,18 @@ echo "</select>";
 
 <?php
 /*student sent*/
-include './functionsphp/dbcheck.inc.php';
-$msg = $_POST['msg'];
+
+$msg2 = $_POST['msg'];
 $reciveris = $_POST['faculty'];
 //$reciveris2 = $_GET['faculty'];
-session_start();
-$me=$_SESSION['pid'];
+
+
 $type=6;
 echo "<script>console.log('$me')</script>";
-echo "<script>console.log('$msg')</script>";
+echo "<script>console.log('$msg2')</script>";
 echo "<script>console.log('reciver'+'$reciveris')</script>";
 if(isset($_POST['submit'])){
-$sq="insert into msgholder 	(senterid,reciverid,message,sendertype,status)values('$me','$reciveris','$msg','$type','0')";
+$sq="insert into msgholder 	(senterid,reciverid,message,sendertype,status)values('$me','$reciveris','$msg2','$type','0')";
 if($s=mysqli_query($conn,$sq)){
 	echo"<script>var r = confirm('message sent Would you like to sent more ');if (r == true) {location.href='senting.php';} else{location.href='homes.php';	}</script>";
 
