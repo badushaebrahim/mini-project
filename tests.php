d6054f8b9e36326dae4db6635a05a1ecb19cd3fc
badushaebrahim <63869474+badushaebrahim@users.noreply.github.com>
2021-10-11 22:13:11 +0530

    up

--- tests.php
+++ tests.php
@@ -38,18 +38,16 @@
 		<style type="text/css">			
 			body{
 				font-family: Arial;
-			    margin: 80px 100px 10px 100px;
-			    padding: 0;
 			    color: white;
 			    text-align: center;
-			    background: #555652;
+			    background: white;
 			}
 
 			.container {
 				color: #E8E9EB;
 				background: #222;
 				border: #555652 1px solid;
-				padding: 10px;
+			
 			}
 		</style>
 
